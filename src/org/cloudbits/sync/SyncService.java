49bb591dc0f4c864eacf41ae164e0a0da868f3d4
Mike Novak <michael.novakjr@gmail.com>
2010-10-19 23:07:40 -0400

    added the SyncAdapter shell and renamed the service package to sync.

--- src/org/cloudbits/sync/SyncService.java
+++ src/org/cloudbits/sync/SyncService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cloudbits.service;
+package org.cloudbits.sync;
 
 import android.app.IntentService;
 import android.content.Intent;
